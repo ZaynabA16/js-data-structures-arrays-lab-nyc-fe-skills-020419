6eabc2745f6f16a6f7c6b89a4d300c6adbdee0a9
Zainab Agboola <zagboola@nyit.edu>
2019-03-18 23:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ destructivelyPrependDriver("Bob");
 destructivelyRemoveLastDriver();
 destructivelyRemoveFirstDriver();
 function appendDriver(name){
-let driversCopy = drivers.slice;
+let driversCopy= drivers.slice;
 driversCopy.push(name);
 return; 
 }
 function prependDriver(name){
-let driversCopy = drivers.slice;  
+let driversCopy= drivers.slice;  
 driversCopy.unshift(name);
 return;
 }
 function removeLastDriver(){
-let driversCopy = drivers.slice; 
+let driversCopy= drivers.slice; 
 driversCopy.pop();
 return;
 }
 function removeFirstDriver(){
-let driversCopy = drivers.slice;
+let driversCopy= drivers.slice;
 driversCopy.shift();
 return;
 }
