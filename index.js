8daa5eea6c4570c5647f084eeac3e0e3bfd604e4
Zainab Agboola <zagboola@nyit.edu>
2019-03-18 23:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,22 +15,19 @@ destructivelyAppendDriver("Ralph")
 destructivelyPrependDriver("Bob");
 destructivelyRemoveLastDriver();
 destructivelyRemoveFirstDriver();
+
 let driversCopy= drivers.slice;
 function appendDriver(name){
-
 return driversCopy.push(name);
 }
 function prependDriver(name){
-let driversCopy= drivers.slice;  
 return driversCopy.unshift(name);
 }
 function removeLastDriver(){
-let driversCopy= drivers.slice; 
 driversCopy.pop();
 return;
 }
 function removeFirstDriver(){
-let driversCopy= drivers.slice;
 driversCopy.shift();
 return;
 }
